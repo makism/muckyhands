803e8ab2cc5cb3a49eda6bc15aa55a59d314b884
makis marimpis <makhsm@gmail.com>
2012-01-27 20:27:19 +0200

    Updated include paths.

--- src/compile.sh
+++ src/compile.sh
@@ -1,10 +1,12 @@
 g++ main.cpp muckyhands.cpp httpserver.cpp \
-	-o muckyhands -O0 -fno-inline -g3 -Wall \
-	-I/opt/cpp-netlib-0.9.2 \
-	-I/opt/boost_1_48_0/include \
-	-L/opt/boost_1_48_0/lib \
-	-L/opt/cpp-netlib-0.9.2/libs/network/src \
-	-lboost_system -pthread \
-	-lcppnetlib-uri-parsers \
-	-lcppnetlib-server-parsers \
-	-lboost_program_options 
+    -o muckyhands -O0 -fno-inline -g3 -Wall \
+    -I/opt/cpp-netlib-0.9.2 \
+    -I/opt/boost_1_48_0/include \
+    -I../3rdparty/json_spirit/include/ \
+    -L/opt/boost_1_48_0/lib \
+    -L/opt/cpp-netlib-0.9.2/libs/network/src \
+    -lboost_system \
+    -lcppnetlib-uri-parsers \
+    -lcppnetlib-server-parsers \
+    -lboost_program_options \
+    -pthread
